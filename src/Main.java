c53ca07363627a07a05563319e6c21337d9d4be3
kea-daron <daron3327@gmail.com>
2025-04-19 22:06:05 +0700

    Homework 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
